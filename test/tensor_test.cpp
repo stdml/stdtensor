b1879a4e30a39ee01c531bbac4fa5e5990a1aeba
lg <lg4869@outlook.com>
2018-10-01 16:14:28 +0800

    add an example

--- test/tensor_test.cpp
+++ test/tensor_test.cpp
@@ -21,8 +21,20 @@ void test_1()
     assert(sum == n * (n + 1) / 2);
 }
 
+void test_2()
+{
+    using pixel_t = std::array<uint8_t, 3>;
+    static_assert(sizeof(pixel_t) == 3, "invalid pixel size");
+    using bmp_t = matrix<pixel_t>;
+
+    int h = 1024;
+    int w = 768;
+    bmp_t img(h, w);  // Note that img is fully packed, without row padding
+}
+
 int main()
 {
     test_1();
+    test_2();
     return 0;
 }
