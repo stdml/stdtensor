3233b6e7c066a4f5b30ccd8b3343b894890334a2
lg <lg4869@outlook.com>
2018-10-01 14:33:58 +0800

    init basic_tensor_ref

--- include/bits/std_tensor.hpp
+++ include/bits/std_tensor.hpp
@@ -4,9 +4,9 @@
 #include <bits/std_shape.hpp>
 
 template <typename R, rank_t r> struct basic_tensor;
-// template <typename R, rank_t r> struct tensor_iterator;
-// template <typename R, rank_t r> struct tensor_ref;
-// template <typename R, rank_t r> struct tensor_view;
+template <typename R, rank_t r> struct basic_tensor_ref;
+// template <typename R, rank_t r> struct basic_tensor_iterator;
+// template <typename R, rank_t r> struct basic_tensor_view;
 
 template <typename R, rank_t r> class basic_tensor
 {
@@ -22,4 +22,20 @@ template <typename R, rank_t r> class basic_tensor
   private:
     const shape<r> shape_;
     const std::unique_ptr<R[]> data_;
+};
+
+template <typename R, rank_t r> class basic_tensor_ref
+{
+  public:
+    template <typename... D>
+    constexpr explicit basic_tensor_ref(R *data, D... d)
+        : shape_(d...), data_(data)
+    {
+    }
+
+    template <typename... I> R &at(I... i) { return data_[shape_.index(i...)]; }
+
+  private:
+    const shape<r> shape_;
+    R *const data_;
 };
--- include/stdtensor.hpp
+++ include/stdtensor.hpp
@@ -1,4 +1,17 @@
 #pragma once
 #include <bits/std_tensor.hpp>
 
+namespace ttl
+{
 template <typename R, rank_t r> using tensor = basic_tensor<R, r>;
+template <typename R, rank_t r> using tensor_ref = basic_tensor_ref<R, r>;
+
+// Don't be confused with std::vector
+template <typename R> using vector = tensor<R, 1>;
+template <typename R> using vector_ref = tensor_ref<R, 1>;
+
+template <typename R> using matrix = tensor<R, 2>;
+template <typename R> using matrix_ref = tensor_ref<R, 2>;
+}  // namespace ttl
+
+using namespace ttl;
