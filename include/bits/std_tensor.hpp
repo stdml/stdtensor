4b92d5a89809a00e313c84e2caf1959643ec3005
lg <lg4869@outlook.com>
2018-10-01 20:21:45 +0800

    array synatx for tensor and tensor_view

--- include/bits/std_tensor.hpp
+++ include/bits/std_tensor.hpp
@@ -25,7 +25,13 @@ template <typename R, typename shape_t> class basic_tensor_ref<R, 0, shape_t>
 
 template <typename R, typename shape_t> class basic_tensor_view<R, 0, shape_t>
 {
-    // TODO
+  public:
+    basic_tensor_view(const R *data) : data_(data) {}
+
+    basic_tensor_view(const R *data, const shape_t &) : data_(data) {}
+
+    //   private:
+    const R *const data_;
 };
 
 template <typename R, typename shape_t>
@@ -34,38 +40,13 @@ R &scalar(const basic_tensor_ref<R, 0, shape_t> &t)
     return t.data_[0];
 }
 
-/* rank > 0 */
-
-template <typename R, rank_t r, typename shape_t = basic_shape<r>>
-class basic_tensor
+template <typename R, typename shape_t>
+R scalar(const basic_tensor_view<R, 0, shape_t> &t)
 {
-    // using subspace_ref_t =
-    //     basic_tensor_ref<R, r - 1, typename shape_t::template subshape_t<1>>;
+    return t.data_[0];
+}
 
-  public:
-    template <typename... D>
-    constexpr explicit basic_tensor(D... d)
-        : shape_(d...), data_(new R[shape_.size()])
-    {
-    }
-
-    R *data() { return data_.get(); }
-
-    const R *data() const { return data_.get(); }
-
-    shape_t shape() const { return shape_; }
-
-    template <typename... I> R &at(I... i)
-    {
-        return data_[shape_.offset(i...)];
-    }
-
-    // subspace_ref_t operator[](int i) const { return ref(*this)[i]; }
-
-  private:
-    const shape_t shape_;
-    const std::unique_ptr<R[]> data_;
-};
+/* rank > 0 */
 
 template <typename R, rank_t r, typename shape_t = basic_shape<r>>
 class basic_tensor_ref
@@ -108,6 +89,9 @@ class basic_tensor_ref
 template <typename R, rank_t r, typename shape_t = basic_shape<r>>
 class basic_tensor_view
 {
+    using subspace_t =
+        basic_tensor_view<R, r - 1, typename shape_t::template subshape_t<1>>;
+
   public:
     template <typename... D>
     constexpr explicit basic_tensor_view(const R *data, D... d)
@@ -115,9 +99,57 @@ class basic_tensor_view
     {
     }
 
+    constexpr explicit basic_tensor_view(const R *data, const shape_t &shape)
+        : shape_(shape), data_(data)
+    {
+    }
+
+    const R *data() const { return data_; }
+
     template <typename... I> R at(I... i) { return data_[shape_.offset(i...)]; }
+
+    subspace_t operator[](int i) const
+    {
+        return subspace_t(data_ + i * shape_.subspace_size(),
+                          shape_.subshape());
+    }
 
   private:
     const shape_t shape_;
     const R *const data_;
+};
+
+template <typename R, rank_t r, typename shape_t = basic_shape<r>>
+class basic_tensor
+{
+    using subspace_ref_t =
+        basic_tensor_ref<R, r - 1, typename shape_t::template subshape_t<1>>;
+
+  public:
+    template <typename... D>
+    constexpr explicit basic_tensor(D... d)
+        : shape_(d...), data_(new R[shape_.size()])
+    {
+    }
+
+    R *data() { return data_.get(); }
+
+    const R *data() const { return data_.get(); }
+
+    shape_t shape() const { return shape_; }
+
+    template <typename... I> R &at(I... i)
+    {
+        return data_[shape_.offset(i...)];
+    }
+
+    subspace_ref_t operator[](int i) const
+    {
+        return subspace_ref_t(data_.get() + i * shape_.subspace_size(),
+                              shape_.subshape());
+    }
+
+  private:
+    const shape_t shape_;
+    const std::unique_ptr<R[]> data_;
 };
--- test/tensor_test.cpp
+++ test/tensor_test.cpp
@@ -33,40 +33,58 @@ void test_2()
     bmp_t img(h, w);  // Note that img is fully packed, without row padding
 }
 
-void test_3()
+template <bool write, typename T> struct test_assign_ {
+    void operator()(T &x, int v) { x = v; }
+};
+
+template <typename T> struct test_assign_<false, T> {
+    void operator()(T &x, int v) { x = v; }
+};
+
+template <bool write, typename T> void test_assign(T &&x, int v)
 {
-    tensor<int, 5> t(3, 4, 5, 6, 7);
+    test_assign_<write, T>()(x, v);
+}
+
+template <typename T, bool write = true> struct test_5d_array {
+    void operator()(const T &t)
     {
-        // TODO: implement
-        // t[1];
-        // t[1][2];
-        // t[1][2][3];
-        // t[1][2][3][4];
-        // t[1][2][3][4][5];
-    }
-    tensor_ref<int, 5> r(t.data(), t.shape());
-    {
-        r[1][2];
-        r[1][2][3];
-        r[1][2][3][4];
-    }
-    {
-        int idx = 0;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                for (int k = 0; k < 5; ++k) {
-                    for (int l = 0; l < 6; ++l) {
-                        for (int m = 0; m < 7; ++m) {
-                            scalar(r[i][j][k][l][m]) = ++idx;
+        t[1];
+        t[1][2];
+        t[1][2][3];
+        t[1][2][3][4];
+        t[1][2][3][4][5];
+
+        {
+            int idx = 0;
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 4; ++j) {
+                    for (int k = 0; k < 5; ++k) {
+                        for (int l = 0; l < 6; ++l) {
+                            for (int m = 0; m < 7; ++m) {
+                                test_assign<write>(scalar(t[i][j][k][l][m]),
+                                                   ++idx);
+                            }
                         }
                     }
                 }
             }
+            int n = 3 * 4 * 5 * 6 * 7;
+            int tot = std::accumulate(t.data(), t.data() + n, 0);
+            if (write) { assert(n * (n + 1) / 2 == tot); }
         }
-        int n = 3 * 4 * 5 * 6 * 7;
-        int tot = std::accumulate(r.data(), r.data() + n, 0);
-        assert(n * (n + 1) / 2 == tot);
     }
+};
+
+void test_3()
+{
+    tensor<int, 5> t(3, 4, 5, 6, 7);
+    tensor_ref<int, 5> r(t.data(), t.shape());
+    tensor_view<int, 5> v(t.data(), t.shape());
+
+    test_5d_array<decltype(t)>()(t);
+    test_5d_array<decltype(r)>()(r);
+    test_5d_array<decltype(v), false>()(v);
 }
 
 int main()
