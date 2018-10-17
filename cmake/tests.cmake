ee37f6ed9187b6f9f4ae00a561ca9d7619484019
lg <lg4869@outlook.com>
2018-10-18 06:24:33 +0800

    update cmake

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -6,17 +6,21 @@ SET(GTEST_GIT_URL
     STRING
     "URL for clone gtest")
 
+SET(PREFIX ${CMAKE_SOURCE_DIR}/3rdparty)
+
 EXTERNALPROJECT_ADD(libgtest-dev
                     GIT_REPOSITORY
                     ${GTEST_GIT_URL}
+                    PREFIX
+                    ${PREFIX}
                     CMAKE_ARGS
-                    -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/3rdparty
+                    -DCMAKE_INSTALL_PREFIX=${PREFIX}
                     -DCMAKE_CXX_FLAGS=-std=c++11
                     -Dgtest_disable_pthreads=1
                     -DBUILD_GMOCK=0)
 
-INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/3rdparty/include)
-LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/3rdparty/lib)
+INCLUDE_DIRECTORIES(${PREFIX}/include)
+LINK_DIRECTORIES(${PREFIX}/lib)
 
 ADD_EXECUTABLE(all_tests
                tests/shape_test.cpp
