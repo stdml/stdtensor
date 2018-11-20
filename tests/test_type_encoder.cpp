23a53272a48cf9a81c3ad6d75aa7c2fe682fe1d1
lg <lg4869@outlook.com>
2018-11-21 01:08:01 +0800

    fix test warning

--- tests/test_type_encoder.cpp
+++ tests/test_type_encoder.cpp
@@ -7,17 +7,19 @@ TEST(type_encoder_test, test1)
     using encoder = std::experimental::basic_type_encoder<
         ttl::internal::idx_format::encoding>;
 
-    ASSERT_EQ(encoder::value<uint8_t>(), 8);
-    ASSERT_EQ(encoder::value<int8_t>(), 9);
-    ASSERT_EQ(encoder::value<int16_t>(), 11);
-    ASSERT_EQ(encoder::value<int32_t>(), 12);
-    ASSERT_EQ(encoder::value<float>(), 13);
-    ASSERT_EQ(encoder::value<double>(), 14);
+    using V = encoder::value_type;
 
-    ASSERT_EQ(encoder::size(8), 1);   // u8
-    ASSERT_EQ(encoder::size(9), 1);   // i8
-    ASSERT_EQ(encoder::size(11), 2);  // i16
-    ASSERT_EQ(encoder::size(12), 4);  // i32
-    ASSERT_EQ(encoder::size(13), 4);  // f32
-    ASSERT_EQ(encoder::size(14), 8);  // f64
+    ASSERT_EQ(encoder::value<uint8_t>(), static_cast<V>(8));
+    ASSERT_EQ(encoder::value<int8_t>(), static_cast<V>(9));
+    ASSERT_EQ(encoder::value<int16_t>(), static_cast<V>(11));
+    ASSERT_EQ(encoder::value<int32_t>(), static_cast<V>(12));
+    ASSERT_EQ(encoder::value<float>(), static_cast<V>(13));
+    ASSERT_EQ(encoder::value<double>(), static_cast<V>(14));
+
+    ASSERT_EQ(encoder::size(8), static_cast<size_t>(1));   // u8
+    ASSERT_EQ(encoder::size(9), static_cast<size_t>(1));   // i8
+    ASSERT_EQ(encoder::size(11), static_cast<size_t>(2));  // i16
+    ASSERT_EQ(encoder::size(12), static_cast<size_t>(4));  // i32
+    ASSERT_EQ(encoder::size(13), static_cast<size_t>(4));  // f32
+    ASSERT_EQ(encoder::size(14), static_cast<size_t>(8));  // f64
 }
