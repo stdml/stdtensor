d082dae0baafce2abe5e9facbb788b6c422fc9ab
LG <lgarithm@users.noreply.github.com>
2019-02-11 22:06:08 +0800

    initial support for cuda tensor (#21) (fix #19)
    * #19 (WIP)
    
    * add fake_device for testing
    
    * show cmake flags
    
    * link cudart

---
+++ tests/fake_cuda_runtime.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <map>
+#include <stdexcept>
+
+class fake_device
+{
+    std::map<const void *, size_t> _allocs;
+
+    void check_leak() const
+    {
+        if (not _allocs.empty()) {
+            throw std::runtime_error("device memory leak detected.");
+        }
+    }
+
+    void check_alloc(const void *data, size_t size) const
+    {
+        const auto pos = _allocs.find(data);
+        if (pos == _allocs.end()) { throw std::runtime_error("not allocated"); }
+        if (pos->second != size) {
+            throw std::runtime_error("alloc size not match");
+        }
+    }
+
+  public:
+    ~fake_device() { check_leak(); }
+
+    void *alloc(size_t size)
+    {
+        void *ptr = malloc(size);
+        _allocs[ptr] = size;
+        return ptr;
+    }
+
+    void free(void *data)
+    {
+        if (_allocs.count(data) == 0) {
+            throw std::runtime_error("invalid free");
+        }
+        _allocs.erase(data);
+    }
+
+    static constexpr int h2d = 1;
+    static constexpr int d2h = 2;
+
+    void memcpy(void *dst, const void *src, int size, int direction) const
+    {
+        switch (direction) {
+        case h2d:
+            check_alloc(dst, size);
+            return;
+        case d2h:
+            check_alloc(src, size);
+            return;
+        default:
+            throw std::runtime_error("invalid memcpy direction");
+        }
+    }
+};
+
+fake_device fake_cuda;
+
+template <typename T> void cudaMalloc(T **ptr, int count)
+{
+    *ptr = (T *)fake_cuda.alloc(sizeof(T) * count);
+}
+
+void cudaFree(void *ptr) { fake_cuda.free(ptr); }
+
+constexpr int cudaMemcpyHostToDevice = fake_device::h2d;
+constexpr int cudaMemcpyDeviceToHost = fake_device::d2h;
+
+void cudaMemcpy(void *dst, const void *src, int size, int direction)
+{
+    fake_cuda.memcpy(dst, src, size, direction);
+}
---
+++ tests/test_cuda_tensor.cpp
@@ -0,0 +1,27 @@
+#include "testing.hpp"
+
+#ifdef USE_FAKE_CUDA_RUNTIME
+#include "fake_cuda_runtime.h"
+#endif
+
+#include <ttl/bits/std_cuda_tensor.hpp>
+#include <ttl/tensor>
+
+using ttl::tensor;
+using ttl::internal::cuda_tensor;
+
+TEST(cuda_tensor_test, test1)
+{
+    using R = float;
+    cuda_tensor<R, 2> m1(10, 100);
+}
+
+TEST(cuda_tensor_test, test2)
+{
+    using R = float;
+    cuda_tensor<R, 2> m1(10, 100);
+    tensor<R, 2> m2(10, 100);
+
+    m1.fromHost(m2.data());
+    m1.toHost(m2.data());
+}
