6bd0479b79cc0862c5e12350a23a9f6db125556a
LG <lgarithm@users.noreply.github.com>
2019-06-26 13:53:04 +0800

    breaking change :: make dims a method (#35)
    * make dims a method
    
    * make dims a method of basic_raw_shape
    
    * add tests

--- include/ttl/bits/std_raw_shape.hpp
+++ include/ttl/bits/std_raw_shape.hpp
@@ -22,42 +22,43 @@ std::vector<D_out> arr2vec(const std::array<D_in, r> &a)
 template <typename Dim = uint32_t> class basic_raw_shape
 {
     using dim_t = Dim;
+    const std::vector<dim_t> dims_;
 
   public:
     using dimension_type = Dim;
 
     template <typename... D>
-    explicit basic_raw_shape(D... d) : dims({static_cast<dim_t>(d)...})
+    explicit basic_raw_shape(D... d) : dims_({static_cast<dim_t>(d)...})
     {
         static_assert(sizeof...(D) <= 0xff, "too many dimensions");
     }
 
-    explicit basic_raw_shape(const std::vector<dim_t> &dims) : dims(dims) {}
+    explicit basic_raw_shape(const std::vector<dim_t> &dims) : dims_(dims) {}
 
     template <rank_t r, typename D>
     explicit basic_raw_shape(const basic_shape<r, D> &shape)
-        : dims(std::move(arr2vec<Dim, D, r>(shape.dims)))
+        : dims_(std::move(arr2vec<Dim, D, r>(shape.dims())))
     {
     }
 
-    rank_t rank() const { return dims.size(); }
+    rank_t rank() const { return dims_.size(); }
 
     dim_t size() const
     {
-        return std::accumulate(dims.begin(), dims.end(), static_cast<dim_t>(1),
-                               std::multiplies<dim_t>());
+        return std::accumulate(dims_.begin(), dims_.end(),
+                               static_cast<dim_t>(1), std::multiplies<dim_t>());
     }
 
     template <rank_t r> basic_shape<r, dim_t> as_ranked() const
     {
         // TODO: use contracts of c++20
         if (r != rank()) { throw std::invalid_argument("invalid rank"); }
-        std::array<dim_t, r> d;
-        std::copy(dims.begin(), dims.end(), d.begin());
-        return basic_shape<r, dim_t>(d);
+        std::array<dim_t, r> dims;
+        std::copy(dims_.begin(), dims_.end(), dims.begin());
+        return basic_shape<r, dim_t>(dims);
     }
 
-    const std::vector<dim_t> dims;
+    const std::vector<dim_t> &dims() const { return dims_; }
 };
 }  // namespace internal
 }  // namespace ttl
