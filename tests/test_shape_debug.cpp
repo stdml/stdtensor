f847d04051c9249bc9330138c4c08f224ffe2839
lg <lg4869@outlook.com>
2019-06-27 13:20:37 +0800

    test more dim types

--- tests/test_shape_debug.cpp
+++ tests/test_shape_debug.cpp
@@ -12,22 +12,37 @@ void test_shape_to_string(const std::string &str,
     ASSERT_EQ(str, ttl::to_string(rs));
 }
 
-TEST(shape_debug_test, test1)
+template <typename D> void test_shape_to_string_all()
 {
     {
-        const auto s = ttl::make_shape();
+        const auto s = ttl::internal::basic_shape<0, D>();
         test_shape_to_string("()", s);
     }
     {
-        const auto s = ttl::make_shape(2);
+        const auto s = ttl::internal::basic_shape<1, D>(2);
         test_shape_to_string("(2)", s);
     }
     {
-        const auto s = ttl::make_shape(2, 3);
+        const auto s = ttl::internal::basic_shape<2, D>(2, 3);
         test_shape_to_string("(2, 3)", s);
     }
     {
-        const auto s = ttl::make_shape(2, 3, 4);
+        const auto s = ttl::internal::basic_shape<3, D>(2, 3, 4);
         test_shape_to_string("(2, 3, 4)", s);
     }
+}
+
+TEST(shape_debug_test, test1)
+{
+    // FIXME: support i8/u8
+
+    // test_shape_to_string_all<int8_t>();
+    test_shape_to_string_all<int16_t>();
+    test_shape_to_string_all<int32_t>();
+    test_shape_to_string_all<int64_t>();
+
+    // test_shape_to_string_all<uint8_t>();
+    test_shape_to_string_all<uint16_t>();
+    test_shape_to_string_all<uint32_t>();
+    test_shape_to_string_all<uint64_t>();
 }
