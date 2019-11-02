1f1550213a530d93479f6b58d6b0259a1ca6e625
lg <lg4869@outlook.com>
2019-11-02 15:53:47 +0800

    --build-gbench

---
+++ cmake/gbench.cmake
@@ -0,0 +1,31 @@
+OPTION(BUILD_GBENCH "Build benchmark from source." OFF)
+
+IF(BUILD_GBENCH)
+    INCLUDE(ExternalProject)
+
+    SET(GBENCH_GIT_URL
+        https://github.com/google/benchmark.git
+        CACHE STRING "URL for clone google benchmark")
+
+    SET(PREFIX ${CMAKE_SOURCE_DIR}/3rdparty)
+
+    EXTERNALPROJECT_ADD(gbench-repo
+                        GIT_REPOSITORY ${GBENCH_GIT_URL}
+                        PREFIX ${PREFIX}
+                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${PREFIX}
+                                   -DCMAKE_BUILD_TYPE=Release
+                                   -DCMAKE_CXX_FLAGS=-std=c++11
+                                   -DBENCHMARK_ENABLE_TESTING=0)
+    LINK_DIRECTORIES(${PREFIX}/lib)
+
+    FUNCTION(TARGET_USE_GBENCH target)
+        TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${PREFIX}/include)
+        TARGET_LINK_LIBRARIES(${target}
+                              benchmark
+                              benchmark_main
+                              Threads::Threads)
+        ADD_DEPENDENCIES(${target} gbench-repo)
+    ENDFUNCTION()
+ELSE()
+    # TODO: find benchmark
+ENDIF()
