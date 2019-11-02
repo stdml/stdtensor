73155c87b42311ac4be1e96be53ac4a39d613311
lg <lg4869@outlook.com>
2019-11-02 15:43:23 +0800

    --build-gtest

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,29 +1,8 @@
-INCLUDE(ExternalProject)
-
-SET(GTEST_GIT_URL https://github.com/google/googletest.git
-    CACHE STRING "URL for clone gtest")
-
-SET(PREFIX ${CMAKE_SOURCE_DIR}/3rdparty)
-
-EXTERNALPROJECT_ADD(libgtest-dev
-                    GIT_REPOSITORY
-                    ${GTEST_GIT_URL}
-                    PREFIX
-                    ${PREFIX}
-                    CMAKE_ARGS
-                    -DCMAKE_INSTALL_PREFIX=${PREFIX}
-                    -DCMAKE_CXX_FLAGS=-std=c++11
-                    -Dgtest_disable_pthreads=1
-                    -DBUILD_GMOCK=0)
-
-LINK_DIRECTORIES(${PREFIX}/lib)
+INCLUDE(${CMAKE_SOURCE_DIR}/cmake/gtest.cmake)
 
 FUNCTION(ADD_GTEST target)
     ADD_EXECUTABLE(${target} ${ARGN} tests/main.cpp)
-    TARGET_LINK_LIBRARIES(${target} gtest)
-    ADD_DEPENDENCIES(${target} libgtest-dev)
-    TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${PREFIX}/include)
-    TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${CMAKE_SOURCE_DIR}/include)
+    TARGET_USE_GTEST(${target})
     TARGET_LINK_LIBRARIES(${target} stdtensor)
     IF(HAVE_CUDA)
         TARGET_LINK_LIBRARIES(${target} cudart)
