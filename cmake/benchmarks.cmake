185b9af4b0865c9dfa140ebd01a8d85ea1c10ce1
LG <lgarithm@users.noreply.github.com>
2019-11-02 23:22:57 +0800

    don't require building gtest and benchmark (#62)

--- cmake/benchmarks.cmake
+++ cmake/benchmarks.cmake
@@ -1,35 +1,16 @@
-INCLUDE(ExternalProject)
-
-SET(GBENCH_GIT_URL https://github.com/google/benchmark.git
-    CACHE STRING "URL for clone google benchmark")
-
-SET(PREFIX ${CMAKE_SOURCE_DIR}/3rdparty)
-
-EXTERNALPROJECT_ADD(gbench-repo
-                    GIT_REPOSITORY
-                    ${GBENCH_GIT_URL}
-                    PREFIX
-                    ${PREFIX}
-                    CMAKE_ARGS
-                    -DCMAKE_INSTALL_PREFIX=${PREFIX}
-                    -DCMAKE_BUILD_TYPE=Release
-                    -DCMAKE_CXX_FLAGS=-std=c++11
-                    -DBENCHMARK_ENABLE_TESTING=0)
-
-LINK_DIRECTORIES(${PREFIX}/lib)
+INCLUDE(${CMAKE_SOURCE_DIR}/cmake/gbench.cmake)
 
 ADD_CUSTOM_TARGET(benchmarks)
 
-FIND_PACKAGE(Threads REQUIRED)
 FUNCTION(ADD_BENCH target)
     ADD_EXECUTABLE(${target} ${ARGN})
-    TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${PREFIX}/include)
-    TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${CMAKE_SOURCE_DIR}/include)
-    TARGET_LINK_LIBRARIES(${target} benchmark benchmark_main Threads::Threads)
+    TARGET_USE_GBENCH(${target})
+    IF(UNIX)
+        TARGET_LINK_LIBRARIES(${target} Threads::Threads)
+    ENDIF()
     IF(HAVE_CUDA)
         TARGET_LINK_LIBRARIES(${target} cudart)
     ENDIF()
-    ADD_DEPENDENCIES(${target} gbench-repo)
     ADD_DEPENDENCIES(benchmarks ${target})
 ENDFUNCTION()
 
---
+++ cmake/gtest.cmake
@@ -0,0 +1,36 @@
+OPTION(BUILD_GTEST "Build gtest from source." OFF)
+
+IF(BUILD_GTEST)
+    INCLUDE(ExternalProject)
+
+    SET(GTEST_GIT_URL
+        https://github.com/google/googletest.git
+        CACHE STRING "URL for clone gtest")
+
+    SET(PREFIX ${CMAKE_SOURCE_DIR}/3rdparty)
+    LINK_DIRECTORIES(${PREFIX}/lib)
+    LINK_DIRECTORIES(${PREFIX}/lib64)
+
+    EXTERNALPROJECT_ADD(libgtest-dev-repo
+                        GIT_REPOSITORY ${GTEST_GIT_URL}
+                        PREFIX ${PREFIX}
+                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${PREFIX}
+                                   -DCMAKE_CXX_FLAGS=-std=c++11
+                                   -Dgtest_disable_pthreads=1
+                                   -DBUILD_GMOCK=0)
+
+    LINK_DIRECTORIES(${PREFIX}/lib)
+
+    FUNCTION(TARGET_USE_GTEST target)
+        TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${PREFIX}/include)
+        TARGET_LINK_LIBRARIES(${target} gtest)
+        ADD_DEPENDENCIES(${target} libgtest-dev-repo)
+    ENDFUNCTION()
+ELSE()
+    FIND_PACKAGE(GTest REQUIRED)
+
+    FUNCTION(TARGET_USE_GTEST target)
+        TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${GTEST_INCLUDE_DIRS})
+        TARGET_LINK_LIBRARIES(${target} ${GTEST_BOTH_LIBRARIES})
+    ENDFUNCTION()
+ENDIF()
