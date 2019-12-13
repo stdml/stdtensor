aa78763ed3beb4bda555482753cd063204f8ea6f
lg <lg4869@outlook.com>
2019-12-13 00:20:11 +0000

    static_assert

--- include/ttl/bits/std_tensor.hpp
+++ include/ttl/bits/std_tensor.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <ttl/bits/std_device.hpp>
 #include <ttl/bits/std_shape.hpp>
 #include <ttl/bits/std_tensor_fwd.hpp>
 #include <ttl/bits/std_tensor_mixin.hpp>
@@ -20,9 +21,17 @@ class basic_tensor<R, basic_shape<0, Dim>, D, owner>
 
     basic_tensor(const basic_shape<0, Dim> &) : basic_tensor() {}
 
-    R operator=(const R &val) const { return *this->data() = val; }
+    R operator=(const R &val) const
+    {  // FIXME: support other devices
+        static_assert(std::is_same<D, host_memory>::value, "");
+        return *this->data() = val;
+    }
 
-    operator R &() const { return *this->data(); }
+    operator R &() const
+    {  // FIXME: support other devices
+        static_assert(std::is_same<D, host_memory>::value, "");
+        return *this->data();
+    }
 };
 
 template <typename R, typename Dim, typename D>
@@ -38,9 +47,17 @@ class basic_tensor<R, basic_shape<0, Dim>, D, readwrite>
     {
     }
 
-    R operator=(const R &val) const { return *this->data() = val; }
+    R operator=(const R &val) const
+    {  // FIXME: support other devices
+        static_assert(std::is_same<D, host_memory>::value, "");
+        return *this->data() = val;
+    }
 
-    operator R &() const { return *this->data(); }
+    operator R &() const
+    {  // FIXME: support other devices
+        static_assert(std::is_same<D, host_memory>::value, "");
+        return *this->data();
+    }
 };
 
 template <typename R, typename Dim, typename D>
@@ -61,7 +78,11 @@ class basic_tensor<R, basic_shape<0, Dim>, D, readonly>
     {
     }
 
-    operator const R &() const { return *this->data(); }
+    operator const R &() const
+    {  // FIXME: support other devices
+        static_assert(std::is_same<D, host_memory>::value, "");
+        return *this->data();
+    }
 };
 
 // rank > 0
