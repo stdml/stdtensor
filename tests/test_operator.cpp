1d817e701619ab53298c5e6ea80e9b45ca741f8c
lg <lg4869@outlook.com>
2019-12-14 20:08:59 +0000

    #47

---
+++ tests/test_operator.cpp
@@ -0,0 +1,23 @@
+#include "testing.hpp"
+
+#include <ttl/operator>
+#include <ttl/tensor>
+
+TEST(range_test, test_1)
+{
+    ttl::tensor<int, 1> x(1);
+    using namespace ttl::operators;
+    auto r = &x;
+    auto v1 = !x;
+    auto v2 = !r;
+    auto v3 = !&x;
+
+    static_assert(std::is_same<decltype(r), ttl::tensor_ref<int, 1>>::value,
+                  "");
+    static_assert(std::is_same<decltype(v1), ttl::tensor_view<int, 1>>::value,
+                  "");
+    static_assert(std::is_same<decltype(v2), ttl::tensor_view<int, 1>>::value,
+                  "");
+    static_assert(std::is_same<decltype(v3), ttl::tensor_view<int, 1>>::value,
+                  "");
+}
