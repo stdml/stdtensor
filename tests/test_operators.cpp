1149c7b7f3b7787db56f378b3d6a67fa4542f853
lg <lg4869@outlook.com>
2019-12-17 20:13:27 +0000

    swap ref and view ops

--- tests/test_operators.cpp
+++ tests/test_operators.cpp
@@ -7,10 +7,10 @@ TEST(operators_test, test_1)
 {
     ttl::tensor<int, 1> x(1);
     using namespace ttl::operators;
-    auto r = &x;
-    auto v1 = !x;
-    auto v2 = !r;
-    auto v3 = !&x;
+    auto r = !x;
+    auto v1 = &x;
+    auto v2 = &r;
+    auto v3 = &!x;
 
     static_assert(std::is_same<decltype(r), ttl::tensor_ref<int, 1>>::value,
                   "");
