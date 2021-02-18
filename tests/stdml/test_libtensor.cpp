42cee86e036fd93d23e05d3f6fcd42571f1ab7ef
lg <lg4869@outlook.com>
2021-02-18 21:52:57 +0000

    fix compare

--- tests/stdml/test_libtensor.cpp
+++ tests/stdml/test_libtensor.cpp
@@ -33,12 +33,12 @@ TEST(libtensor_test, test_show)
     {
         ml::Tensor x(ml::i32, ml::Shape());
         const auto s = ml::show(x);
-        ASSERT_EQ(s, "i32()0");
+        ASSERT_EQ(s, std::string("i32()0"));
     }  // namespace ml=stdml;
     {
         ml::Tensor x(ml::i32, ttl::make_shape(2, 3));
         std::iota(x.data<int32_t>(), x.data<int32_t>() + x.size(), 0);
         const auto s = ml::show(x);
-        ASSERT_EQ(s, "i32(2, 3)[[0, 1, 2], [3, 4, 5]]");
+        ASSERT_EQ(s, std::string("i32(2, 3)[[0, 1, 2], [3, 4, 5]]"));
     }
 }
