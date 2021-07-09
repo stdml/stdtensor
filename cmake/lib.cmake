ac3ffc34ec5b0ae079223f67ead71a62c4bbc20e
lg <lg4869@outlook.com>
2021-07-09 11:03:44 +0100

    POSITION_INDEPENDENT_CODE ON

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -5,6 +5,7 @@ INSTALL(TARGETS stdtensor ARCHIVE DESTINATION lib)
 # libtensor will be shared by python/haskell bindings
 FILE(GLOB_RECURSE srcs ${CMAKE_SOURCE_DIR}/src/stdml/*)
 ADD_LIBRARY(tensor ${srcs})
+SET_TARGET_PROPERTIES(tensor PROPERTIES POSITION_INDEPENDENT_CODE ON)
 TARGET_LINK_LIBRARIES(tensor dl)
 
 INSTALL(TARGETS tensor ARCHIVE DESTINATION lib)
