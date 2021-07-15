d65d3be48b7058cf81595733a3ba0d33ff70ccf0
lg <lg4869@outlook.com>
2021-07-15 12:36:27 +0100

    ttl::lookup

---
+++ tests/test_type_traits.cpp
@@ -0,0 +1,20 @@
+#include "testing.hpp"
+
+#include <ttl/type_traits>
+
+TEST(type_traits_test, test_1)
+{
+    using Ts = std::tuple<int, float>;
+    {
+        constexpr int pos = ttl::lookup<int, Ts>::value;
+        ASSERT_EQ(pos, 0);
+    }
+    {
+        constexpr int pos = ttl::lookup<float, Ts>::value;
+        ASSERT_EQ(pos, 1);
+    }
+    {  // won't compile
+       // constexpr int pos = ttl::lookup_type<double, Ts>::value;
+       // ASSERT_EQ(pos, 2);
+    }
+}
