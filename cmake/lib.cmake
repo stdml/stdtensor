5410cd0998bfad9a71f23e4942b1943d7bcf63bd
lg <lg4869@outlook.com>
2021-07-24 18:10:55 +0100

    remove tensor.h

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -1,12 +1,9 @@
-# TODO: deprecate
-ADD_LIBRARY(stdtensor src/tensor.cpp)
-INSTALL(TARGETS stdtensor ARCHIVE DESTINATION lib)
-
-# libtensor will be shared by python/haskell bindings
-FILE(GLOB_RECURSE srcs ${CMAKE_SOURCE_DIR}/src/stdml/*)
-ADD_LIBRARY(tensor ${srcs})
-SET_TARGET_PROPERTIES(tensor PROPERTIES POSITION_INDEPENDENT_CODE ON)
-TARGET_LINK_LIBRARIES(tensor dl)
-
-INSTALL(TARGETS tensor ARCHIVE DESTINATION lib)
-SET_PROPERTY(TARGET tensor PROPERTY CXX_STANDARD 17)
+FUNCTION(BUILD_STDML_TENSOR_LIB target)
+    # libtensor will be shared by python/haskell bindings
+    FILE(GLOB_RECURSE srcs ${CMAKE_SOURCE_DIR}/src/stdml/*)
+    ADD_LIBRARY(${target} ${srcs})
+    SET_TARGET_PROPERTIES(${target} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+    TARGET_LINK_LIBRARIES(${target} dl)
+    SET_PROPERTY(TARGET ${target} PROPERTY CXX_STANDARD 17)
+    INSTALL(TARGETS ${target} ARCHIVE DESTINATION lib)
+ENDFUNCTION()
