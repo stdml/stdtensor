2d0d501b6098ca6c10154338fecb11708acd4101
lg <lg4869@outlook.com>
2021-08-03 00:26:18 +0100

    basic_factory_t

---
+++ include/ttl/bits/std_factory.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <functional>
+#include <map>
+
+namespace ttl
+{
+namespace internal
+{
+template <typename T, typename A>
+using constructor_t = std::function<T(A)>;
+
+template <typename T, typename A, typename K>
+class basic_factory_t
+{
+    using constructor = constructor_t<T, A>;
+    std::map<K, constructor> constructors_;
+
+  public:
+    void operator()(K k, constructor c)
+    {
+        if (constructors_.count(k) > 0) {
+            throw std::invalid_argument("registering duplicated constructor");
+        }
+        constructors_.emplace(std::move(k), std::move(c));
+    }
+
+    bool contains(K k) const { return constructors_.count(k) > 0; }
+
+    constructor operator[](K k) const { return constructors_.at(k); }
+
+    T operator()(K k, A a) const
+    {
+        if (constructors_.count(k) <= 0) {
+            throw std::invalid_argument(
+                "constructor not registered");  // FIXME: show k
+        }
+        return constructors_.at(k)(std::move(a));
+    }
+};
+}  // namespace internal
+}  // namespace ttl
