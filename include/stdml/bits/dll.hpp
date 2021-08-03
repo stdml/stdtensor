9a03f430dacf2a54c850acf98c83f2b20d298302
lg <lg4869@outlook.com>
2021-08-03 01:55:22 +0100

    dll

---
+++ include/stdml/bits/dll.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+namespace stdml
+{
+class dll
+{
+    void *handle_;
+
+  public:
+    dll(std::string name, std::string prefix = "");
+
+    void *raw_sym(const std::string &name) const;
+
+    template <typename T>
+    T sym(const std::string &name) const
+    {
+        return reinterpret_cast<T>(raw_sym(name));
+    }
+};
+}  // namespace stdml
---
+++ src/stdml/dll.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <stdexcept>
+#include <stdml/bits/dll.hpp>
+
+#include <dlfcn.h>
+
+namespace stdml
+{
+static constexpr int mode = RTLD_LAZY;
+
+dll::dll(std::string name, std::string prefix)
+{
+    std::string soname = "lib" + name + ".so";
+    std::string path = prefix + soname;
+    handle_ = dlopen(path.c_str(), mode);
+    if (handle_ == nullptr) { throw std::runtime_error(dlerror()); }
+}
+
+void *dll::raw_sym(const std::string &name) const
+{
+    void *f = dlsym(handle_, name.c_str());
+    if (f == nullptr) {
+        fprintf(stderr, "%s not defined\n", name.c_str());
+        throw std::runtime_error(std::string("dlsym failed: ") + dlerror());
+    }
+    return f;
+}
+}  // namespace stdml
