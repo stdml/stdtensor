52b495b467fb2b8d28ac7177bccffe121fde855c
lg <lg4869@outlook.com>
2021-08-08 20:08:41 +0100

    fix

--- tests/stdml/test_detect.cpp
+++ tests/stdml/test_detect.cpp
@@ -1,9 +1,16 @@
 #include "../testing.hpp"
 
-#include <stdml/bits/tensor/allocator.hpp>
+#include <stdml/bits/tensor/cudart.hpp>
 
 TEST(libtensor_test, test_detect)
 {
     auto ok = stdml::has_cuda();
     printf("%d\n", ok);
+    int n = stdml::get_cuda_gpu_count();
+    if (!ok) {
+        ASSERT_EQ(n, 0);
+    } else {
+        // FIXME: maybe == 0
+        ASSERT_TRUE(n > 0);
+    }
 }
