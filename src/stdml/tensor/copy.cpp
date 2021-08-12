5dfe6b3f9c0652c2ac22ff6866ca63c5a2840cfb
lg <lg4869@outlook.com>
2021-08-12 23:33:02 +0100

    u

--- src/stdml/tensor/copy.cpp
+++ src/stdml/tensor/copy.cpp
@@ -1,29 +1,38 @@
 #include <stdml/bits/tensor/allocator.hpp>
+#include <stdml/bits/tensor/cudart.hpp>
 #include <stdml/bits/tensor/io.hpp>
 #include <ttl/consistent_variable>
 
 namespace stdml
 {
-void copy(const TensorRef &y, const TensorView &x)
+namespace ops
 {
-    // ttl::consistent_variable<size_t> n(x.size());
-    // n = y.size();
-    // using GA = generic_allocator;
-    // if (x.device() == cpu && y.device() == cpu) {
-    //     GA::_h2h(y.data(), x.data(), n);
-    //     return;
-    // }
-    // if (x.device() == cpu && y.device() == cpu) {
-    //     GA::_h2h(y.data(), x.data(), n);
-    //     return;
-    // }
-    // if (x.device() == cpu && y.device() == cpu) {
-    //     GA::_h2h(y.data(), x.data(), n);
-    //     return;
-    // }
-    // if (x.device() == cpu && y.device() == cpu) {
-    //     GA::_h2h(y.data(), x.data(), n);
-    //     return;
-    // }
-}
+class copy
+{
+  public:
+    void operator()(const TensorRef &y, const TensorView &x) const
+    {
+        const size_t n = x.data_size();
+        if (n != y.data_size()) {
+            throw std::invalid_argument("copy: inconsistent size");
+        }
+        if (x.device() == cpu && y.device() == cpu) {
+            std::memcpy(y.data(), x.data(), n);
+            return;
+        }
+        auto &l = libcudart::get();
+        if (x.device() == cpu && y.device() == cuda) {
+            l.from_host(y.data(), x.data(), n);
+        } else if (x.device() == cuda && y.device() == cpu) {
+            l.to_host(y.data(), x.data(), n);
+        } else if (x.device() == cuda && y.device() == cuda) {
+            l.d2d(y.data(), x.data(), n);
+        } else {
+            throw std::invalid_argument("stdml::ops::copy");
+        }
+    }
+};
+}  // namespace ops
+
+void copy(const TensorRef &y, const TensorView &x) { ops::copy()(y, x); }
 }  // namespace stdml
