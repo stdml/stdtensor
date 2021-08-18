6f2cd11d2ecec2c8bc1105d3c89e0effb8d2874f
lg <lg4869@outlook.com>
2021-08-18 22:02:03 +0100

    SHARED

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -1,9 +1,9 @@
 FUNCTION(BUILD_STDML_TENSOR_LIB target)
     # libtensor will be shared by python/haskell bindings
     FILE(GLOB_RECURSE srcs ${CMAKE_SOURCE_DIR}/src/stdml/*)
-    ADD_LIBRARY(${target} ${srcs})
+    ADD_LIBRARY(${target} SHARED ${srcs})
     SET_TARGET_PROPERTIES(${target} PROPERTIES POSITION_INDEPENDENT_CODE ON)
     TARGET_LINK_LIBRARIES(${target} dl)
     SET_PROPERTY(TARGET ${target} PROPERTY CXX_STANDARD 17)
-    INSTALL(TARGETS ${target} ARCHIVE DESTINATION lib)
+    INSTALL(TARGETS ${target} DESTINATION lib)
 ENDFUNCTION()
